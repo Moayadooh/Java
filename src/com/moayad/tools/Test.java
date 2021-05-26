45e40307af5054fe32e5ea11c6c4d8e754f33686
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-05-26 14:54:43 +0400

    ***

--- src/com/moayad/tools/Test.java
+++ src/com/moayad/tools/Test.java
@@ -3,12 +3,22 @@ package com.moayad.tools;
 public class Test {
 
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i=0;i<1000;i++){
-            stringBuilder.append(i+" ");
+        String word = "aallaa";
+        int i = 0;
+        int j = word.length() - 1;
+        boolean isPal = true;
+        while (j>i){
+            if(word.charAt(i) != word.charAt(j)){
+                isPal = false;
+                break;
+            }
+            i++;
+            j--;
         }
-        String numbers = stringBuilder.toString();
-        System.out.println(numbers);
+        if(isPal)
+            System.out.println("Is a palindrome.");
+        else
+            System.out.println("Is not a palindrome.");
     }
 
 }
